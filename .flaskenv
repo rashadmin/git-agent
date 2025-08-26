d8d7aaefc877844e5a7ab529e90d43a73c436ed5
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-08-26 21:23:58 +0100

    created git agent

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=git_agent.py
+FLASK_ENV=development
---
+++ app/agent_call/graph.py
@@ -0,0 +1,50 @@
+from typing_extensions import TypedDict,List, Dict, Any, Annotated, Optional
+from dataclasses import dataclass
+from langgraph.graph import StateGraph, END
+from langgraph.types import Command
+from langgraph.runtime import Runtime
+from app.agent_call.external import r
+from langgraph.checkpoint.memory import MemorySaver
+
+# ---- State Definition ----
+def add(left, right):
+    return left + right
+
+# class RequestEntry(TypedDict):
+#     id: str
+#     data: Dict[str, Any]
+
+# @dataclass
+# class ContextSchema(TypedDict):
+#     respond: bool = False
+
+@dataclass
+class AgentState(TypedDict):
+    date: str
+    commits:Annotated[List[dict],add]
+    # requests: Annotated[list[RequestEntry], add]
+    # selected_request: Optional[int] = None
+
+def receiver_node(state:AgentState):
+    print(state)
+    return {}
+
+# # ---- Node: Responder ----
+# def responder(state: AgentState):
+#     selected_request_id = state.get("selected_request", None)
+#     if runtime.context.get("respond", False):
+#         if selected_request_id is not None:
+#             selected_request = state["requests"][selected_request_id]
+#             response_stream_id = selected_request["data"]["reply_to"]
+#             r.xadd(response_stream_id, {"response": "Yes"})
+#             return {}
+#     return {}
+
+# ---- Graph Definition ----
+builder = StateGraph(AgentState)
+builder.add_node(receiver_node)
+# builder.add_node("responder", responder)
+builder.set_entry_point("receiver_node")
+# builder.add_edge("responder", END)
+checkpointer = MemorySaver()
+graph = builder.compile(checkpointer=checkpointer)
