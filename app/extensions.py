2e67a224794717cd9a6623d60febc20f03fa3714
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-08-31 17:40:11 +0100

    added from conn

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,9 @@
 # app/extensions.py
 from langgraph.checkpoint.postgres import PostgresSaver
+import psycopg
 
 DB_URI = "postgresql://git_agent_db_user:Jg8FAmsZRmRNWPsSowBFfNTCD40bwQ4S@dpg-d2q621f5r7bs73abgs6g-a.oregon-postgres.render.com/git_agent_db?sslmode=require"
+conn = psycopg.connect(DB_URI)
 
-checkpointer = PostgresSaver(DB_URI)
+checkpointer = PostgresSaver(conn)
 checkpointer.setup()  # make sure schema exists
