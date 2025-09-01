48a16814187f79f9b61d7eb41648e8237268ad31
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-01 13:28:50 +0100

    added autocommit

--- app/extensions.py
+++ app/extensions.py
@@ -3,7 +3,7 @@ from langgraph.checkpoint.postgres import PostgresSaver
 import psycopg
 
 DB_URI = "postgresql://git_agent_db_user:Jg8FAmsZRmRNWPsSowBFfNTCD40bwQ4S@dpg-d2q621f5r7bs73abgs6g-a.oregon-postgres.render.com/git_agent_db?sslmode=require"
-conn = psycopg.connect(DB_URI)
+conn = psycopg.connect(DB_URI,autocommit=True)
 
 checkpointer = PostgresSaver(conn)
 # checkpointer.setup()  # make sure schema exists
