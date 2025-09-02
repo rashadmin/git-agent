819127b925358fa71a09febab4b3f4970549dfcd
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-02 14:08:02 +0100

    REVERT TO DEFAULT

--- app/agent_call/routes.py
+++ app/agent_call/routes.py
@@ -7,7 +7,7 @@ import requests
 from app.agent_call import bp
 from datetime import datetime,timedelta
 from langgraph.types import Command
-from app.extensions import pool
+from app.extensions import checkpointer,conn
 
 
 # Start listener thread when app launches
@@ -15,15 +15,6 @@ from app.extensions import pool
 # listener_thread.start()
 
 
-def get_checkpoints():
-    with pool.connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("""
-                SELECT thread_id, checkpoint_id, checkpoint
-                FROM checkpoints
-                ORDER BY checkpoint_id DESC
-            """)
-            return cur.fetchall()
 
 
 @bp.route("/health",methods=['GET'])
@@ -54,15 +45,15 @@ def run_agent():
     
 @bp.route("/compose", methods=["GET"])
 def compose_text():
+    cur = conn.cursor()
     printer = []
-    rows = get_checkpoints()
+    cur.execute("SELECT thread_id, checkpoint_id, checkpoint FROM checkpoints ORDER BY checkpoint_id DESC")
+    rows = cur.fetchall()
     today_thread_id = {row[0] for row in rows if datetime.fromisoformat(row[2]['ts']).date() == datetime.now().date()}
     # compose for every commit for that day per repo by :
     # we will query the db for the checkpointer to return all threads that was modified the previous day
     for thread_id in today_thread_id:
         printer.extend(text_composer(thread_id))
-
-        # commit only if you modify da    
     return printer
     # return state.values['extracted_commits']
 
--- app/extensions.py
+++ app/extensions.py
@@ -1,11 +1,9 @@
 # app/extensions.py
 from langgraph.checkpoint.postgres import PostgresSaver
 import psycopg
-from psycopg_pool import ConnectionPool
 
 DB_URI = "postgresql://git_agent_db_xocq_user:HRsa5HU2jL4y52lml80ZsmvG6dsjhlBF@dpg-d2r9j86r433s73fbnoa0-a.oregon-postgres.render.com/git_agent_db_xocq?sslmode=require"
-pool = ConnectionPool(DB_URI, min_size=1, max_size=5, max_lifetime=1800)
+conn = psycopg.connect(DB_URI,autocommit=True)
 
-conn = pool.connection()
 checkpointer = PostgresSaver(conn)
 # checkpointer.setup()  # make sure schema exists
