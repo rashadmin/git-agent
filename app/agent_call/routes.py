166529a35ab2431ed7e76f1610839cff15c4cf73
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-07 23:07:40 +0100

    fixed posts bug to post

--- app/agent_call/routes.py
+++ app/agent_call/routes.py
@@ -60,7 +60,7 @@ def compose_text():
     for thread_id in today_thread_id:
         text_composer(thread_id)
         repo = bytes.fromhex(thread_id).decode("utf-8")
-        cur.execute(f'SELECT date_committed FROM post where repo ="{repo}"')
+        cur.execute(f'SELECT date_commited FROM post where repo ="{repo}"')
         config = {"configurable": {"thread_id": thread_id}}
         state = graph.get_state(config=config).values
         date_in_db = cur.fetchall()
