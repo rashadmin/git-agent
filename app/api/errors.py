b609f93d75e13e0362070db83c02f4bb0307b2f1
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-07 09:11:56 +0100

    added session rollback

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,13 +1,27 @@
-from flask import jsonify
-from werkzeug.http import HTTP_STATUS_CODES
-
-def error_response(status_code, message=None):
-    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+from werkzeug.http import HTTP_STATUS_CODES 
+from app.api import bp
+from werkzeug.exceptions import HTTPException
+from app import db
+def error_response(status_code,message=None):
+    payload = {'error':HTTP_STATUS_CODES.get(status_code,'unknown_error')}
     if message:
         payload['message'] = message
-    response = jsonify(payload)
-    response.status_code = status_code
-    return response
+    return payload,status_code
+
+
 
 def bad_request(message):
-    return error_response(500, message)
+    return error_response(400,message)
+
+@bp.errorhandler(HTTPException)
+def handle_exception(e):
+    return error_response(e.code)
+
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    return error_response(404)
+
+@bp.app_errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return error_response(500)
