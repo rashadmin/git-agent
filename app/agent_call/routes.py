f298cc432b8855c0eb154c3c07eda3e0af9a7ef0
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-12 10:21:36 +0100

    added debugger

--- app/agent_call/routes.py
+++ app/agent_call/routes.py
@@ -56,7 +56,7 @@ def compose_text():
     cur.execute("SELECT thread_id, checkpoint_id, checkpoint FROM checkpoints ORDER BY checkpoint_id DESC")
     rows = cur.fetchall()
     yesterday = datetime.now().date() - timedelta(days=1)
-    today_thread_id = {row[0] for row in rows if datetime.fromisoformat(row[2]['ts']).date() == yesterday}
+    today_thread_id = {row[0] for row in rows if datetime.fromisoformat(row[2]['ts']).date() == datetime.now().date()}
     # compose for every commit for that day per repo by :
     # we will query the db for the checkpointer to return all threads that was modified the previous day
     for thread_id in today_thread_id:
