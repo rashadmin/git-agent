862b83168bf80599f957c2607187b4c04f59b151
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-15 01:13:25 +0100

    maybe solved

--- app/agent_call/external.py
+++ app/agent_call/external.py
@@ -69,14 +69,14 @@ def get_all_commits(payload):
         response = requests.get(url,headers=headers)
         print(url)
         print(response.status_code)
-        if response.status_code==200:
-            datum = response.json()
+        if response.status_code!=200:
+            break
+        datum = response.json()
+        if not datum:  # stop when no more commits
+            break
 
-            if not datum:  # stop when no more commits
-                break
-
-            data.extend(datum)
-            page += 1
+        data.extend(datum)
+        page += 1
     # url = f"{GITHUB_API_URL}/repos/{repo}/commits"
 
     thread_id = payload['repository']['full_name'].encode("utf-8").hex()
