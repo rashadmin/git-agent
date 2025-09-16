a7c97126da60c715ffac41377efa2bd0a608f11d
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-16 11:09:28 +0100

    ...

--- app/agent_call/graph.py
+++ app/agent_call/graph.py
@@ -19,6 +19,7 @@ import os
 from pydantic import BaseModel,Field
 from typing import Optional,List,Set
 from langgraph.checkpoint.postgres import PostgresSaver
+from app.extensions import graph_context
 
 # ---- State Definition ----
 def add(left, right):
@@ -125,8 +126,12 @@ def extraction_node(state:AgentState):
         extracted_commit = structured_llm.invoke(commit_prompt)
         extracted_commit = [add_date(date,file) for file in extracted_commit.model_dump()['repository']]
         # extracted_commits.extend(extracted_commit)
-        yield {'extracted_commits':extracted_commit}
-
+        DB_URI = current_app.config['SQLALCHEMY_DATABASE_URI']
+        thread_id = state['commits']['repository']['full_name'].encode("utf-8").hex()
+        with graph_context(DB_URI) as graph:
+            graph.update_state(
+            {"configurable": {"thread_id": thread_id}},
+            {'extracted_commits':extracted_commit})
     # extract all the date in formatted using pandas
     # slice through df for each date, using each date run the extract and extend the extracted_commit list
     # a looop start#
--- git_agent.py
+++ git_agent.py
@@ -1,12 +1,15 @@
 from app import create_app,db
 from app.models import User,Post
 from app.agent_call.composer import run_compose
-from app.extensions import graph_context
+from app.agent_call.graph import builder
+from langgraph.checkpoint.postgres import PostgresSaver
+import psycopg
 app = create_app()
 DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
 
 @app.shell_context_processor
 def make_shell_context():
-    with graph_context(DB_URI) as graph:
-        # state = graph.get_state(config=config).values
-        return {'graph':graph,'user':User,'post':Post,'run':run_compose}
+    conn = psycopg.connect(DB_URI, autocommit=True)
+    checkpointer = PostgresSaver(conn)
+    graph = builder.compile(checkpointer=checkpointer)
+    return {'graph':graph,'user':User,'post':Post,'run':run_compose}
