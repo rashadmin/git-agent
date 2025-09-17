2f6964e46191d13f0e68ad0d59a67476e3b53bf5
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-17 12:31:52 +0100

    ...

--- app/agent_call/graph.py
+++ app/agent_call/graph.py
@@ -118,23 +118,24 @@ def extraction_node(state:AgentState):
     unique_commit_date = df['day'].unique()
     print('unique commit')
     print(unique_commit_date)
-    from app.extensions import graph_context
-    with graph_context() as graph:
-        for date in unique_commit_date:
-            temp = df[df['day']==date]
-            temp_message = temp['message']
-            # temp_date = temp['commit_date'].dt.dayofyear
-            commit_prompt = prompt_template.invoke({'text_string':temp_message.tolist()})#change from state to df slice
-            print('Im here\n\n\n\n\n\n')
-            structured_llm = llm.with_structured_output(schema=Repository)
-            extracted_commit = structured_llm.invoke(commit_prompt)
-            extracted_commit = [add_date(date,file) for file in extracted_commit.model_dump()['repository']]
-            # extracted_commits.extend(extracted_commit)
-            DB_URI = current_app.config['SQLALCHEMY_DATABASE_URI']
-            thread_id = state['commits']['repository']['full_name'].encode("utf-8").hex()
-            print(extracted_commit)
-            import logging
-            logging.basicConfig(level=logging.INFO)
+    for date in unique_commit_date:
+        temp = df[df['day']==date]
+        temp_message = temp['message']
+        # temp_date = temp['commit_date'].dt.dayofyear
+        commit_prompt = prompt_template.invoke({'text_string':temp_message.tolist()})#change from state to df slice
+        print('Im here\n\n\n\n\n\n')
+        structured_llm = llm.with_structured_output(schema=Repository)
+        extracted_commit = structured_llm.invoke(commit_prompt)
+        extracted_commit = [add_date(date,file) for file in extracted_commit.model_dump()['repository']]
+        # extracted_commits.extend(extracted_commit)
+        DB_URI = current_app.config['SQLALCHEMY_DATABASE_URI']
+        thread_id = state['commits']['repository']['full_name'].encode("utf-8").hex()
+        from app.extensions import graph_context
+        print(extracted_commit)
+        import logging
+
+        logging.basicConfig(level=logging.INFO)
+        with graph_context() as graph:
             logging.info(f"[BEFORE update_state] {pool.get_stats()}")
             print('line 136')
             graph.update_state(
