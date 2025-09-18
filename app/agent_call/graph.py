3a90e5fc1ab96dee733b0343ae1e5bb31584f0c8
rashadmin <abdul.a.rasheed2021@gmail.com>
2025-09-18 08:38:50 +0100

    WORKING?

--- app/agent_call/graph.py
+++ app/agent_call/graph.py
@@ -29,21 +29,21 @@ def add(left, right):
     return left
 
 def adder(left,right):
-    # if len(left)==0:
-    left.extend(right)
-    #     return left
-    # df = pd.DataFrame().from_records(left)
-    # df_updates = pd.DataFrame().from_records(right)
-    # if df_updates.shape[0]>0:
-    # # Merge with updates (outer join keeps everything)
-    #     df = df.merge(df_updates, on=["commit_id", "filename"], how="outer", suffixes=("", "_new"))
+    if len(left)==0:
+        left.extend(right)
+        return left
+    df = pd.DataFrame().from_records(left)
+    df_updates = pd.DataFrame().from_records(right)
+    if df_updates.shape[0]>0:
+    # Merge with updates (outer join keeps everything)
+        df = df.merge(df_updates, on=["commit_id", "filename"], how="outer", suffixes=("", "_new"))
 
-    #     # If compiled_new exists, prefer it, else keep old compiled
-    #     df["compiled"] = df["compiled_new"].combine_first(df["compiled"])
+        # If compiled_new exists, prefer it, else keep old compiled
+        df["compiled"] = df["compiled_new"].combine_first(df["compiled"])
 
-    # # Drop the helper column
-    #     df = df.drop(columns=[col for col in df.columns if "new" in col])
-    # left = df.to_dict(orient='records')
+    # Drop the helper column
+        df = df.drop(columns=[col for col in df.columns if "new" in col])
+    left = df.to_dict(orient='records')
     return left
     
 
@@ -105,8 +105,7 @@ def extraction_node(state:AgentState):
     df = pd.DataFrame().from_records(state['formatted_commits'])
     print(df.head())
     extracted_commits_df = pd.DataFrame().from_records(state.get('extracted_commits',[]))
-    # if extracted_commits_df.shape[0] > 0:
-    if False:
+    if extracted_commits_df.shape[0] > 0:
         df["commit_id"] = df["message"].str.split("Commit_id").str[1].str.split(",").str[0].str.strip().str[2:]
         df = df[~df["commit_id"].isin(extracted_commits_df["commit_id"])]
         df.drop('commit_id',axis=1,inplace=True)
@@ -125,12 +124,10 @@ def extraction_node(state:AgentState):
         # temp_date = temp['commit_date'].dt.dayofyear
         commit_prompt = prompt_template.invoke({'text_string':temp_message.tolist()})#change from state to df slice
         print('Im here\n\n\n\n\n\n')
-        # structured_llm = llm.with_structured_output(schema=Repository)
-        # extracted_commit = structured_llm.invoke(commit_prompt)
-        # extracted_commit = [add_date(date,file) for file in extracted_commit.model_dump()['repository']]
-        # extracted_commits.extend(extracted_commit)
-        import time
-        time.sleep(15)
+        structured_llm = llm.with_structured_output(schema=Repository)
+        extracted_commit = structured_llm.invoke(commit_prompt)
+        extracted_commit = [add_date(date,file) for file in extracted_commit.model_dump()['repository']]
+        extracted_commits.extend(extracted_commit)
         DB_URI = current_app.config['SQLALCHEMY_DATABASE_URI']
         thread_id = state['commits']['repository']['full_name'].encode("utf-8").hex()
         extracted_commit = [{'date':date}]
